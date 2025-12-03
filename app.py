1efc8d17b45cbffc56537968b791708aa7e23156
Ashwin-KBRC <ashwin.it@kbrc.com.kw>
2025-12-03 08:50:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -667,8 +667,9 @@ elif mode == "Analytics":
 # FOOTER
 # ========================================
 st.sidebar.markdown("---")
-st.sidebar.write("Contact Mr. Ashwin Joseph Mathew, Head of IT, for assistance or regarding Technical issues")
+st.sidebar.write("Contact Eng. Ashwin Joseph Mathew, Head of IT, for assistance or regarding Technical issues")
 st.sidebar.write("Mail : Ashwin.IT@kbrc.com.kw")
+
 
 
 
